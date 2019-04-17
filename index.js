51905bca6602bbfb407ac7152dc70709ba219f48
Martyna <martyna.glaz@gmail.com>
2019-04-17 02:30:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 var customerName="bob"
 function upperCaseCustomerName(customerName){
-console.log(customerName.toUpperCase());
+var lover = (customerName.toUpperCase());
 
 }
