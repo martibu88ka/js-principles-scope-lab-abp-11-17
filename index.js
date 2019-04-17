263e2b8eb7392fe579ac9c38d535b1e6036dab99
Martyna <martyna.glaz@gmail.com>
2019-04-17 02:12:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution in this file!
 customerName="bob"
 function upperCaseCustomerName(customerName){
-  return .toUpperCase(customerName);
+  return customerName.toUpperCase();
 }
