559c59f03e843a7d08ca1392ff809dd31b6158a4
Martyna <martyna.glaz@gmail.com>
2019-04-17 02:47:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Write your solution in this file!
 var customerName="bob"
-var bestCustomer
+
 function upperCaseCustomerName(){
   customerName = customerName.toUpperCase()
 }
 function setBestCustomer(){
-var bestCustomer = "not bob"
+var bestCustomer = bobby
+return bestCustomer="not bob"
 }
