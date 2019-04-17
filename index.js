98999651c43e7d0db803469df368370667f27fad
Martyna <martyna.glaz@gmail.com>
2019-04-17 02:23:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 var customerName="bob"
 function upperCaseCustomerName(customerName){
-  let capital=customerName.toUpperCase()
+  let customerName.toUpperCase()
   return capital;
 }
