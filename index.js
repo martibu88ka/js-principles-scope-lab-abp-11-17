fcce6c53d75bffef76f5b7d6195c924ea88ddd94
Martyna <martyna.glaz@gmail.com>
2019-04-17 02:46:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,8 @@
 var customerName="bob"
 
 function upperCaseCustomerName(){
-  customerName= customerName.toUpperCase()
+  customerName = customerName.toUpperCase()
+}
+function setBestCustomer(){
+var bestCustomer === "not bob"  
 }
